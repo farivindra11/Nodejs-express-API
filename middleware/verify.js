a7d36e92c2b3fbd31a060827085d966d0d66dfae
fariv indra <farivind@gmail.com>
2021-02-05 09:34:32 +0700

    add midlleware

---
+++ middleware/verify.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+
+const signUp = async (req, res) => {
+    try {
+        // this field for check duplucate username when user sign up
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const signIn = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { signUp, signIn }
