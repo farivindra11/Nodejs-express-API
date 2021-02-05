a99773a18622a363553f465a43ad044ce2c8a2f7
fariv indra <farivind@gmail.com>
2021-02-05 09:13:45 +0700

    updated user router

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController')
-
+const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', userController.getAll);
-router.post('/register', userController.createUser);
+router.get("/", userController.getAll);
+router.post("/register", userController.createUser);
+router.get("/:id_user", userController.getById);
+router.put("/:id_user", userController.updateUser);
 
 module.exports = router;
