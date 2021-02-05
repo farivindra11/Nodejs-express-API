802026ab95d613c7afe32a9fe65e1ec4e8e06243
fariv indra <farivind@gmail.com>
2021-02-05 15:13:59 +0700

    add verify middleware

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const verify = require("../middleware/verify")
 
 /* GET users listing. */
 router.get("/", userController.getAll);
-router.post("/register", userController.createUser);
+router.post("/register",verify.signUp, userController.createUser);
 router.get("/:id_user", userController.getById);
 router.put("/:id_user", userController.updateUser);
 router.delete("/:id_user", userController.deleteUser)
