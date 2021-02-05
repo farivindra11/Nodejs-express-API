afbd1ae53acee7e868f9764f65b9ce3534b8f9b1
fariv indra <farivind@gmail.com>
2021-02-05 15:13:15 +0700

    verify duplicate data

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,9 +1,22 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const db = require('../config/db.config')
 
-const signUp = async (req, res) => {
+const signUp = async (req, res, next) => {
     try {
         // this field for check duplucate username when user sign up
+        const { email, nip } = req.body;
+
+        const data = await db.query(`SELECT * FROM tb_user WHERE email=$1 OR nip=$2`, [email, nip])
+        
+        if(data) {
+            res.status(400).json({
+                message: "error",
+                error: "Email or nip already in use!"
+            })
+        }
+
+
     } catch (error) {
         console.log(error);
     }
